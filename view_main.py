1e64531c8b4caa607893f390fab41040ed93a0d1
dfyz85 <492587@gmail.com>
2011-07-23 21:42:28 +0300

    first

--- view_main.py
+++ view_main.py
@@ -5,3 +5,10 @@ Created on 12.05.2011
 @author: dfyz
 '''
 from django.shortcuts import render_to_response
+
+def home(request):
+    pass
+def contact(request):
+    pass
+def partner(request):
+    pass
